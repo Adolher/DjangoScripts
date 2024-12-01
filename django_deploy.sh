10c15c8d2e810115dd6ab554b1c7aaa0da9f8287
Adalher <adalher@adalher.net>
2024-12-01 20:07:09 +0100

    initial commit

---
+++ django_deploy.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+
+#   Variable Data
+ProjectName="ProjectAdalher"     # supplement your Project Name
+DomainName="adalher.net"      # supplement your Domain name
+Git_ProjectURL="https://github.com/Adolher/ProjectAdalher"  # supplement the repository url
+AdminEMail="admin@adalher.net"      # supplement your E-Mail for ssl-certificate
+RootPath="/webapps"       # supplement your Path to your websites
+ALLOWED_HOSTS="192.168.27.30"
+
+#   Constant Data
+ProjectPath=$RootPath/$ProjectName
+ProjectUser="$ProjectName"_user
+ProjectGroup="$ProjectName"_group
+
+#   File Data
+GunicornStart=$(cat <<EOF
+#!/bin/bash
+
+NAME=$ProjectName
+DJANGODIR=$ProjectPath/$ProjectName
+SOCKFILE=$ProjectPath/run/gunicorn.sock
+USER=$ProjectUser
+GROUP=$ProjectGroup
+NUM_WORKERS=`nproc`
+DJANGO_SETTINGS_MODULE=$ProjectName.settings
+DJANGO_WSGI_MODULE=$ProjectName.wsgi
+TIMEOUT=120
+
+cd \$DJANGODIR
+source ../venv/bin/activate
+export DJANGO_SETTINGS_MODULE=\$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=\$DJANGODIR:\$PYTHONPATH
+
+RUNDIR=\$(dirname \$SOCKFILE)
+test -d \$RUNDIR || mkdir -p \$RUNDIR
+
+exec ../venv/bin/gunicorn \${DJANGO_WSGI_MODULE}:application \
+  --name \$NAME \
+  --workers \$NUM_WORKERS \
+  --timeout \$TIMEOUT \
+  --user=\$USER --group=\$GROUP \
+  --bind=unix:\$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
+EOF
+)
+
+SuperVisor=$(cat <<EOF
+# $ProjectName.conf
+
+[program:$ProjectName]
+command = $ProjectPath/venv/bin/gunicorn_start
+user = $ProjectUser
+stdout_logfile = $ProjectPath/logs/supervisor.log
+redirect_stderr = true
+environment=LANG=en_US.UTF-8,LC_ALL=en_US.UTF-8
+EOF
+)
+
+NginX=$(cat <<EOF
+upstream $ProjectName.app_server {
+    server unix:$ProjectPath/run/gunicorn.sock fail_timeout=0;
+}
+
+server {
+    listen 80;
+    server_name $DomainName;
+    
+    access_log $ProjectPath/logs/access.log;
+    error_log $ProjectPath/logs/error.log;
+    
+    location /static/ {
+        alias $ProjectPath/$ProjectName/static/;
+    }
+    
+    location /media/ {
+        alias $ProjectPath/$ProjectName/media/;
+    }
+    
+    location / {
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        
+        proxy_set_header Host \$http_host;
+        
+        proxy_redirect off;
+        
+        if (!-f \$request_filename) {
+            proxy_pass http://$ProjectName.app_server;
+        }
+    }
+}
+EOF
+)
+
+
+#   1. Prepare Debian for Django deployment
+#   1.1 install packages
+
+# apt update && upgrade
+#apt install nginx
+#apt install certbot
+#apt install python3
+#apt install python3-pip
+#apt install python3-venv 
+#apt install python3-certbot-nginx
+#apt install supervisor
+#apt install git
+
+#   1.2 create user and group
+
+groupadd --system $ProjectGroup
+useradd --system --gid $ProjectGroup --shell /bin/bash --home $ProjectPath $ProjectUser
+
+cd $RootPath
+
+#   2. get and prepare the Django Project
+#   2.1 get the Django project
+git clone $Git_ProjectURL
+
+#   2.2 create and prepare a virtual environment
+cd $ProjectPath
+python3 -m venv venv
+source venv/bin/activate
+pip install -r $RootPath/$ProjectName/requirements.txt     # Todo: Pfad überprüfen
+
+#   2.2.1 migrate Database          # Todo
+cd $ProjectPath/$ProjectName
+#python3 manage.py migrate
+
+#   2.2.2 install and prepare gunicorn
+pip install gunicorn
+echo "$GunicornStart" > $ProjectPath/venv/bin/gunicorn_start
+chmod +x $ProjectPath/venv/bin/gunicorn_start
+
+#   2.2.3 change the ALLOWED_HOSTS
+sed -i "s/\[\]/\['$ALLOWED_HOSTS'\]/" $ProjectPath/$ProjectName/$ProjectName/settings.py
+
+
+#   2.3 prepare supervisor
+mkdir $ProjectPath/logs
+echo "$SuperVisor" > /etc/supervisor/conf.d/$ProjectName.conf
+supervisorctl status
+
+#   2.4 prepare nginx
+echo "$NginX" > /etc/nginx/sites-available/$ProjectName.conf
+cd /etc/nginx/sites-enabled/
+ln -s ../sites-available/$ProjectName.conf
+service nginx start
+service nginx status
+cd $ProjectPath
+
+#   2.5 get ssl certificate
+printf "$AdminEMail\ny\nn\n" | certbot -d $DomainName
+
+#   3 Post-Processing
+chown -R $ProjectUser:$ProjectGroup $ProjectPath
+
+service nginx restart
+service nginx status
+
+supervisorctl restart $ProjectName
+supervisorctl status
---
+++ django_start.sh
@@ -0,0 +1,300 @@
+#!/bin/bash
+
+#   Variable Data
+ProjectName=" "     # supplement your Project Name
+AppName=""          # supplement your App Name
+Git_ProjectURL=""   # supplement your Git_ProjectURL Name
+RootPath="/ "       # supplement your Path to your websites
+
+#   Constant Data
+ProjectPath=$RootPath/$ProjectName
+VenvName="venv"
+
+Template=$(cat <<EOF
+
+EOF
+)
+
+Skeleton=$(cat <<EOF
+{% load static %}
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title> {% block title %}{% endblock %}</title>
+    <link rel="stylesheet", href="{% static "$ProjectName/css/main.css" %}">
+    {% block css_sheets %}{% endblock %}
+</head>
+<body>
+    <div class="header">
+        {% block header %}
+        {% include "header.html" %}
+        {% endblock %}
+    </div>
+    <div class="sidebar">
+        {% block sidebar %}
+        {% include "sidebar.html" %}
+        {% endblock %}
+    </div>
+    <div class="content">
+        {% block content %}
+        {% endblock %}
+    </div>
+    <div class="advertising">
+        {% block advertising %}
+        {% include "advertising.html" %}
+        {% endblock %}
+    </div>
+    <div class="footer">
+        {% block footer %}
+        {% include "footer.html" %}
+        {% endblock %}
+    </div>
+
+    <script src="{% static "$ProjectName/js/main.js" %}"></script>
+    {% block js_scripts %}{% endblock %}
+</body>
+</html>
+
+EOF
+)
+
+Advertising=$(cat <<EOF
+{% block advertising %}
+
+<p>advertising</p>
+
+{% endblock %}
+
+EOF
+)
+
+Footer=$(cat <<EOF
+{% block footer %}
+
+<p>This is the footer of my website.</p>
+
+{% endblock %}
+
+EOF
+)
+
+Header=$(cat <<EOF
+{% block header %}
+
+<h1>$ProjectName</h1>
+
+<p>This is the header of my website.</p>
+
+{% endblock %}
+
+EOF
+)
+
+Sidebar=$(cat <<EOF
+{% block sidebar %}
+
+<p>Sidebar</p>
+
+{% endblock %}
+
+EOF
+)
+
+LandingPageFile=$(cat <<EOF
+{% extends "skeleton.html" %}
+{% load static %}
+
+{% block title %} $ProjectName $AppName {% endblock %}
+{% block css_sheets %}
+<link rel="stylesheet", href="{% static "$AppName/css/$AppName.css" %}">
+{% endblock %}
+
+{% block content %}
+
+<p>Here you will see my website.</p>
+<p>...later</p>
+<p>... maybe in some days...</p>
+<p>... or weeks :p</p>
+
+{% endblock %}
+
+{% block js_scripts %}
+<script src="{% static "$AppName/js/$AppName.js" %}"></script>
+{% endblock %}
+
+EOF
+)
+
+LandingUrls=$(cat <<EOF
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.landingpage, name='landingpage')     # Todo: change landingpage to a variable ?Maybe?
+]
+
+EOF
+)
+
+LandingViews=$(cat <<EOF
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def landingpage(request):
+    template = loader.get_template('landingpage.html')
+    return HttpResponse(template.render())
+
+EOF
+)
+
+MainCss=$(cat <<EOF
+:root{
+    --sc_middle: 2;
+    --ca_middle: 8;
+    --end: 9;
+}
+
+body {
+    max-width: 61%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(12%, 8);
+    gap: 5px;
+
+    background-color: lightseagreen;
+}
+
+.header {
+    grid-column: 1/var(--end);
+    grid-row: 1/2;
+    
+    background-color: rgb(42, 13, 109);
+}
+
+.sidebar {
+    grid-column: 1/var(--sc_middle);
+    grid-row: 2/span 4;
+    
+    background-color: rgb(42, 13, 109);
+}
+
+.content{
+    min-height: 50em;
+    grid-column: var(--sc_middle)/8;
+    grid-row: 2/span 3;
+    
+    background-color: rgb(67, 19, 179);
+}
+
+.advertising{
+    grid-column: var(--ca_middle)/var(--end);
+    grid-row: 2/span 3;
+    
+    background-color: rgb(107, 71, 192);
+}
+
+.footer {
+    grid-column: var(--sc_middle)/var(--end);
+    grid-row: 5/6;
+    
+    background-color: rgb(42, 13, 109);
+}
+
+EOF
+)
+
+LandingCss=$(cat <<EOF
+.content p {
+    color: bisque;
+}
+EOF
+)
+
+Requirements=$(cat <<EOF
+Django
+EOF
+)
+
+GitIgnore=$(cat <<EOF
+venv
+__pycache__
+*.sqlite3
+
+EOF
+)
+
+#   1. Prepare Debian for Django by installing packages.
+# apt update && upgrade
+sudo apt install build-essential
+sudo apt install python3
+sudo apt install python3-pip
+sudo apt install python3-venv
+sudo apt install git
+
+#   2. Create and Prepare an virtual Environment
+mkdir $ProjectPath
+cd $ProjectPath
+
+python3 -m venv $VenvName
+source venv/bin/activate
+
+echo "$Requirements" > $ProjectPath/requirements.txt
+pip install -r requirements.txt
+
+#   3. Create and prepare Project
+django-admin startproject $ProjectName
+
+mkdir $ProjectPath/$ProjectName/$ProjectName/templates
+echo "$Skeleton" > $ProjectPath/$ProjectName/$ProjectName/templates/skeleton.html
+echo "$Advertising" > $ProjectPath/$ProjectName/$ProjectName/templates/advertising.html
+echo "$Footer" > $ProjectPath/$ProjectName/$ProjectName/templates/footer.html
+echo "$Header" > $ProjectPath/$ProjectName/$ProjectName/templates/header.html
+echo "$Sidebar" > $ProjectPath/$ProjectName/$ProjectName/templates/sidebar.html
+
+sed -i "s/'DIRS': \[\]/'DIRS': \[BASE_DIR\/\"$ProjectName\"\/\"templates\"\]/" $ProjectPath/$ProjectName/$ProjectName/settings.py
+sed -i "/STATIC_URL/aSTATICFILES_DIRS = \[ BASE_DIR \/ \"static\" \]" $ProjectPath/$ProjectName/$ProjectName/settings.py
+sed -i "/django.contrib/afrom django.urls import include" $ProjectPath/$ProjectName/$ProjectName/urls.py
+
+#   4. Create App and add some folders and files
+cd $ProjectPath/$ProjectName
+python3 manage.py startapp $AppName
+mkdir $ProjectPath/$ProjectName/$AppName/templates/
+echo "$LandingPageFile" > $ProjectPath/$ProjectName/$AppName/templates/landingpage.html
+echo "$LandingUrls" > $ProjectPath/$ProjectName/$AppName/urls.py
+echo "$LandingViews" > $ProjectPath/$ProjectName/$AppName/views.py
+
+#   4.1 Connect App with Project
+sed -i "/INSTALLED_APPS/a\\\t'$AppName',\n" $ProjectPath/$ProjectName/$ProjectName/settings.py
+sed -i "/urlpatterns = /a\\\tpath('', include('$AppName.urls')),\n" $ProjectPath/$ProjectName/$ProjectName/urls.py
+
+#   5. Create static folder and content
+mkdir $ProjectPath/$ProjectName/static/
+mkdir $ProjectPath/$ProjectName/static/$ProjectName/
+mkdir $ProjectPath/$ProjectName/static/$ProjectName/css
+echo "$MainCss" > $ProjectPath/$ProjectName/static/$ProjectName/css/main.css
+mkdir $ProjectPath/$ProjectName/static/$ProjectName/js
+echo "console.log(\"You are in the Skeleton\", main.js);" > $ProjectPath/$ProjectName/static/$ProjectName/js/main.js
+mkdir $ProjectPath/$ProjectName/static/$ProjectName/media
+
+
+mkdir $ProjectPath/$ProjectName/static/$AppName/
+mkdir $ProjectPath/$ProjectName/static/$AppName/css
+echo "$LandingCss" > $ProjectPath/$ProjectName/static/$AppName/css/$AppName.css
+mkdir $ProjectPath/$ProjectName/static/$AppName/js
+echo "console.log(\"You are on the Landigpage\", $AppName.js);" > $ProjectPath/$ProjectName/static/$AppName/js/$AppName.js
+mkdir $ProjectPath/$ProjectName/static/$AppName/media
+
+# Todo: git
+git init
+git branch -M main
+echo "$GitIgnore" > $ProjectPath/.gitignore
+git add *
+git add .gitignore
+git commit -m "initial commit"
+git remote add origin $Git_ProjectURL
+git push -u origin main
